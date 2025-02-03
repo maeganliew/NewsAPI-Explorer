a77f9806296d8d43f24f4edb36434a40674585b9
Liew Jia Wei <mjw.liew@gmail.com>
2025-02-03 14:08:59 +0800

    bugs

--- src/components/Newsboard.jsx
+++ src/components/Newsboard.jsx
@@ -1,9 +1,14 @@
 
 export const Newsboard = () => {
+
+    const[article,setArtical] = useState([]);
+    useEffect(()=> {
+        let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_API_KEY}`;
+    })
+
   return (
     <div>
-    <h2 className="text-center"> Latest <span className="">News</span></h2>
-        
+    <h2 className="text-center"> Latest <span className="badge bg-danger">News</span></h2>
     </div>
   )
 }
